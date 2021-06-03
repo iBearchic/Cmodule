8aa95471a74617522cba3dee0e59d9c85db351c4
Maksonio <32421746+iBearchic@users.noreply.github.com>
2021-06-04 01:52:16 +0300

    Update mymod.c
    added 3 of 6 checked functions

--- mymod.c
+++ mymod.c
@@ -6,12 +6,12 @@ static PyObject *king(PyObject *self, PyObject *args) {
   int orig, bvi, n;
   double result;
 
-  if (!PyArg_ParseTuple(args,"ddd", &orig, &bvi, &n)) {
+  if (!PyArg_ParseTuple(args,"iii", &orig, &bvi, &n)) {
     return NULL;
   }
   double orig_k = 0.6;
   double bvi_k = 0.4;
-  result = 0.00000001 + (double)((orig * orig_k + bvi * bvi_k ) / n);
+  result = 0.0001 + (double)((orig * orig_k + bvi * bvi_k ) / n);
   return Py_BuildValue("d", result);
 }
 
@@ -28,15 +28,23 @@ static PyObject *kong(PyObject *self, PyObject *args) {
 /* Partition coefficient*/
 static PyObject *kp(PyObject *self, PyObject *args) {
     double result;
-    int *p;
-    if (!PyArg_ParseTuple(args,"l", &p)) {
+    PyObject* p;
+    if (!PyArg_ParseTuple(args,"O", &p)) {
         return NULL;
     }
-    result = (double)(150 - 8.3 * p[0] - 6.0 * p[1]- 4.1 * p[2]) / 100;
+    PyObject* p0 = PySequence_Fast_GET_ITEM(p, 0);
+    PyObject* p1 = PySequence_Fast_GET_ITEM(p, 1);
+    PyObject* p2 = PySequence_Fast_GET_ITEM(p, 2);
+
+    int res0 = PyLong_AsLong(p0);
+    int res1 = PyLong_AsLong(p1);
+    int res2 = PyLong_AsLong(p2);
+
+    result = (double)(150 - 8.3 * res0 - 6.0 * res1 - 4.1 * res2) / 100;
     return Py_BuildValue("d", result);
 }
 
-/* Helping coefficient*/
+/* Helping coefficient
 static PyObject *kd(PyObject *self, PyObject *args) {
     int n;
     int *lst;
@@ -50,8 +58,51 @@ static PyObject *kd(PyObject *self, PyObject *args) {
     }
     temp = temp * 1.001203200114052001;
     return Py_BuildValue("d", temp);
-}
+}*/
 
-/* Probability*/
-static PyObject *p(PyObject *self, PyObject *args) {
+static PyMethodDef ownmod_methods[] = {
+    { 
+        "king", // name of fucntion in python interpreter
+        king, // function C declaration
+        METH_VARARGS, // special macros about function arguments
+        "Functions of serching inner-coefficient" // doc for function in python interpreter
+    },
+    { 
+        "kong", // name of fucntion in python interpreter
+        kong, // function C declaration
+        METH_VARARGS, // special macros about function arguments
+        "Functions of serching outer-coefficient" // doc for function in python interpreter
+    },
+    { 
+        "kp", // name of fucntion in python interpreter
+        kp, // function C declaration
+        METH_VARARGS, // special macros about function arguments
+        "Partition coefficient" // doc for function in python interpreter
+    },
+    { NULL, NULL, 0, NULL }
+};
+
+static PyModuleDef simple_module = {
+    /* Info about module */
+    PyModuleDef_HEAD_INIT,
+    "my_mod", // my_mod.__name__ 
+    "amazing documentation", // my_mod.__doc__ 
+    -1, 
+    ownmod_methods, // methods are here 
+    NULL, 
+    NULL, 
+    NULL, 
+    NULL, 
+    NULL
+};
+
+PyMODINIT_FUNC PyInit_my_mod(void)
+{
+    PyObject* m;
+    // creating module object
+    m = PyModule_Create(&simple_module);
+    if (m == NULL)
+        return NULL;
+
+    return m;
 }
